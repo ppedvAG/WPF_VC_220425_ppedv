08d0d9a19d0b5ccae94226162e2b9d0007987b56
Klaas Hagge <KlaasH@ppedv.de>
2022-04-27 13:08:38 +0200

    Validation

---
+++ Validation/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation
+{
+    public class Person : IDataErrorInfo
+    {
+        private string name;
+
+        public string Name 
+        { 
+            get => name;
+            set 
+            {
+                if (value.All(x => Char.IsLetter(x)))
+                    name = value;
+                else
+                    throw new Exception("Bitte gib nur Buchstaben ein");
+            }
+        }
+
+        public int Alter { get; set; }
+
+
+        public string Error => null;
+
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case nameof(Alter):
+                        if (Alter < 0) return "Du wurdest noch nicht geboren. Falsch!";
+                        if (Alter > 150) return "Du sollst nicht bei deine Alter lügen!";
+                        break;
+                }
+
+                return String.Empty;
+            }
+        }
+    }
+}
