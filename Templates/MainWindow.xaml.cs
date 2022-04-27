6a48c270bdc0afe810cbe7910b68e2099208982f
Klaas Hagge <KlaasH@ppedv.de>
2022-04-27 09:09:13 +0200

    Templates

--- Templates/MainWindow.xaml.cs
+++ Templates/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +21,27 @@ namespace Templates
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Person> Personenliste { get; set; } = new ObservableCollection<Person>()
+        {
+            new Person(){Nachname="Nass", Vorname="Anna", Alter=45},
+            new Person(){Nachname="Zufall", Vorname="Rainer", Alter=67},
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = this;
         }
 
         private void Btn_ControlTemplate_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Button funktioniert");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Personenliste.Remove((sender as Button).Tag as Person);
         }
     }
 }
